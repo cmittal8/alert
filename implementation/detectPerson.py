5ced67a99df1600a66b77f0bdabaab52b63c9b26
Girish Hari <bunnefant@gmail.com>
2021-10-23 16:42:18 -0400

    dist camera speaker files

--- implementation/detectPerson.py
+++ implementation/detectPerson.py
@@ -2,8 +2,9 @@ import io, os, sys
 import json
 import setuptools
 import google.protobuf
+from google.protobuf.json_format import MessageToJson
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "C:/Users/Chiranshi/OneDrive - Georgia Institute of Technology/Gatech/PersonalGit/vaulted-night-329901-629349427f85.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "secret.json"
 
 def detect_faces(path):
     """Detects faces in an image."""
@@ -16,34 +17,12 @@ def detect_faces(path):
 
     image = vision.Image(content=content)
 
-    response = client.object_localization(image=image).localized_object_annotations
-    print(response)
-    #faces = response.face_annotations
+    response = client.object_localization(image=image)
+    response = response.localized_object_annotations
+    for object in response:
+        if object.name == 'Person':
+            return 'Person detected'
 
-    # Names of likelihood from google.cloud.vision.enums
-    # likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
-    #                    'LIKELY', 'VERY_LIKELY')
-    # print('Faces:')
-    # count = 0
-    # for face in faces:
-    #     count = count + 1
-    #     # print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
-    #     # print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
-    #     # print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
-
-    #     # vertices = (['({},{})'.format(vertex.x, vertex.y)
-    #     #             for vertex in face.bounding_poly.vertices])
-
-    #     # print('face bounds: {}'.format(','.join(vertices)))
-    # if (count == 1):
-    #     print("True")
-    # else:
-    #     print(count)
-    #     print("False")
-    if response.error.message:
-        raise Exception(
-            '{}\nFor more info on error messages, check: '
-            'https://cloud.google.com/apis/design/errors'.format(
-                response.error.message))
+    return 'Person not detected'
 if __name__ == '__main__':
-    detect_faces("C:/Users/Chiranshi/Pictures/Camera Roll/WIN_20211023_09_01_05_Pro.jpg")
+    print(detect_faces("image.jpg"))
---
+++ playaudio.py
@@ -0,0 +1,4 @@
+
+import os
+
+os.system('mpg123 message.mp3')
