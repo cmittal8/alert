630fba35194a769f5e5ce21fabebd74c4597d0ca
cmittal8 <72054681+cmittal8@users.noreply.github.com>
2021-10-23 13:17:52 -0400

    AttributeError in detectPerson.py

--- implementation/detectPerson.py
+++ implementation/detectPerson.py
@@ -1,6 +1,8 @@
 import io, os, sys
 import json
 import setuptools
+import google.protobuf
+
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "C:/Users/Chiranshi/OneDrive - Georgia Institute of Technology/Gatech/PersonalGit/vaulted-night-329901-629349427f85.json"
 
 def detect_faces(path):
@@ -14,33 +16,34 @@ def detect_faces(path):
 
     image = vision.Image(content=content)
 
-    response = client.face_detection(image=image)
-    faces = response.face_annotations
+    response = client.object_localization(image=image).localized_object_annotations
+    print(response)
+    #faces = response.face_annotations
 
     # Names of likelihood from google.cloud.vision.enums
     # likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
     #                    'LIKELY', 'VERY_LIKELY')
     # print('Faces:')
-    count = 0
-    for face in faces:
-        count = count + 1
-        # print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
-        # print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
-        # print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
+    # count = 0
+    # for face in faces:
+    #     count = count + 1
+    #     # print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
+    #     # print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
+    #     # print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
 
-        # vertices = (['({},{})'.format(vertex.x, vertex.y)
-        #             for vertex in face.bounding_poly.vertices])
+    #     # vertices = (['({},{})'.format(vertex.x, vertex.y)
+    #     #             for vertex in face.bounding_poly.vertices])
 
-        # print('face bounds: {}'.format(','.join(vertices)))
-    if (count == 1):
-        print("True")
-    else:
-        print(count)
-        print("False")
+    #     # print('face bounds: {}'.format(','.join(vertices)))
+    # if (count == 1):
+    #     print("True")
+    # else:
+    #     print(count)
+    #     print("False")
     if response.error.message:
         raise Exception(
             '{}\nFor more info on error messages, check: '
             'https://cloud.google.com/apis/design/errors'.format(
                 response.error.message))
 if __name__ == '__main__':
-    detect_faces("C:/Users/Chiranshi/Downloads/Kroger Recipt.jpg")
+    detect_faces("C:/Users/Chiranshi/Pictures/Camera Roll/WIN_20211023_09_01_05_Pro.jpg")
